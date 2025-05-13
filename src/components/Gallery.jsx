ca0523b9f0c28e18aa78a51fb3074842eb3f9747
JudithJude369 <ifunanyajudith09@gmail.com>
2025-05-13 11:47:19 +0100

    course

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,8 +1,54 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+// API = "50258272-21da67c9547761fff04b85ae8";
+//
+// using react query for data fetching and displaying images
+const url =
+  "https://pixabay.com/api/?key=50258272-21da67c9547761fff04b85ae8&q=cat&image_type=photo";
+
 const Gallery = () => {
+  const response = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(url);
+      return result.data;
+    },
+  });
+  console.log(response.data);
+  if (response.isPending) {
+    return <h4>Loading...</h4>;
+  }
+  if (response.isError) {
+    return <h4>There was error...</h4>;
+  }
+  if (response.data.length < 1) {
+    return <h4>No results found...</h4>;
+  }
   return (
-    <div className="title-container">
-      <h1 className="title">pixabay images</h1>
-    </div>
+    <section className="image-container">
+      {response.data.hits.map((item) => {
+        return (
+          <div key={item.id}>
+            <img src={item.webformatURL} alt="" className="img" />
+            <div className="text-container">
+              <p>
+                <strong>Image By: </strong>
+                {item.user}
+              </p>
+              <p>
+                {" "}
+                <strong>Tags:</strong> {item.tags}
+              </p>
+              <p>
+                <strong>Resolution:</strong> {item.imageHeight} X{" "}
+                {item.imageWidth}
+              </p>
+            </div>
+          </div>
+        );
+      })}
+    </section>
   );
 };
 
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,5 +1,26 @@
+import { FaSearch } from "react-icons/fa";
+
 const SearchForm = () => {
-  return <div></div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return console.log(searchValue);
+  };
+
+  return (
+    <div className="search-container">
+      <h1 className="title">pixabay images</h1>
+      <form className="form-wrapper" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="input"
+          placeholder="search for free images"
+          name="search"
+        />
+        <FaSearch className="search-icon" />
+      </form>
+    </div>
+  );
 };
 
 export default SearchForm;
